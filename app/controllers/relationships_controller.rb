9acc2d0ed6b71d1c1c7e4b289a42de718a092259
obara134 <tyobara@icloud.com>
2021-04-04 11:24:07 +0000

    update

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,33 +1,22 @@
 class RelationshipsController < ApplicationController
-  before_action :set_user
-
+  before_action :authenticate_user!
   def create
-    following = current_user.follow(@user)
-    if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
-    end
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
   end
-
   def destroy
-    following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
-    end
-  end
-
-  private
-  
-  def 
-    @user = User.find(params[:relationship][:follow_id])
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
   end
   
+  def followings
+     user = User.find(params[:user_id])
+     @users = user.followings
+  end
 
+  def followers
+     user = User.find(params[:user_id])
+     @users = user.followers
+  end
+ 
 end
