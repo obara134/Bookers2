9a749bb036be3273b14d0c770a4275704573fee9
obara134 <tyobara@icloud.com>
2021-04-11 07:07:18 +0000

    update

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,29 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    unless @book_comment.save
+      render 'error'  # app/views/book_comments/error.js.erbを参照する ※要件外
+    end
+    # app/views/book_comments/create.js.erbを参照する
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+    # app/views/book_comments/destroy.js.erbを参照する
   end
 
   private
-
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-
 end
+
+
+
